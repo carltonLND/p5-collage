6860ae0d05c153db87aa524414d1cb0ffd110592
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-12 14:17:02 +0100

    Initial Commit

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+//added from https://github.com/Gaweph/p5-typescript-starter
+// This file will add both p5 instanced and global intellisence
+import * as p5Global from "p5/global";
+import module from "p5";
+
+declare global {
+  const VIDEO: any;
+}
+
+export = module;
+export as namespace p5;
